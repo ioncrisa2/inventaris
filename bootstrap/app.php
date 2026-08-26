1db99b9e43a77e035c2ce90d437180c8e7559f93
root <root@srv1931047.hstgr.cloud>
2026-08-26 06:10:31 +0000

    fix: support HTTPS reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
+
+	$middleware->trustProxies(at: '*');        
+
+	$middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
